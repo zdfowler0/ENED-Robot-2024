c8dbaa7d797022870aff2a2e874f557e30d464b8
zdfowler0 <155563410+zdfowler0@users.noreply.github.com>
2024-02-16 11:54:54 -0500

    Code to run first subtask
    Move forward and backward a specified distance and laps.

---
+++ Subtask 1/SubTask1a.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MoveTank, SpeedPercent, follow_for_ms
+from ev3dev2.sensor.lego import GyroSensor
+import math
+
+# Make controls
+moveDA = MoveTank(OUTPUT_D, OUTPUT_A)
+
+# Initialize gyro sensor
+moveDA.gyro = GyroSensor()
+
+# Calibrate the gyro to initialize the current angle as 0
+moveDA.gyro.calibrate()
+
+# variables determined by subtask
+num_laps = 0
+distance_cm = 0
+
+# variables we want to use
+target_angle = 0
+speed_percent = 30
+distance_mm = distance_cm * 10
+
+# wheel specifications
+wheel_diameter = 68.8
+wheel_circumference = 2*math.pi()*(wheel_diameter / 2)
+rotations = distance_mm / wheel_circumference
+
+# move a set number of rotations
+def move(self, speed, rotations):
+    self.on_for_rotations(speed,speed,rotations=rotations,brake=True)
+
+# run the specified number of laps
+for i in range(num_laps):
+    if(i % 2 == 0):
+        move(moveDA, speed_percent, rotations)
+    else:
+        move(moveDA, -speed_percent, rotations)
+
+'''
+# Follow the target_angle for 4500ms
+moveDA.follow_gyro_angle(kp=11.3, ki=0.05, kd=3.2,speed=SpeedPercent(speed_percent),target_angle=target_angle,follow_for = follow_for_ms,ms=4500)
+'''
