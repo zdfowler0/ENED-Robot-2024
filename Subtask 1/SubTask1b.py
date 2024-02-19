8cbe417dbd60f4141da09abe8d05e7f890edcf7a
zdfowler0 <155563410+zdfowler0@users.noreply.github.com>
2024-02-19 12:04:32 -0500

    Update SubTask1b.py
    Sounds

--- Subtask 1/SubTask1b.py
+++ Subtask 1/SubTask1b.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python3
 
-from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MoveTank, SpeedPercent, follow_for_ms
-import math
+from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MoveTank, SpeedPercent
+from ev3dev2.sound import Sound
 
 # Make controls
 moveDA = MoveTank(OUTPUT_D, OUTPUT_A)
+# Sound
+spkr = Sound()
 
 # variables determined by subtask
-num_laps = 5
-distance_cm = 150
+num_laps = 4
+distance_cm = 90
 
 # variables we want to use
 speed_percent = 30
 distance_mm = distance_cm * 10
 
-# wheel specifications (68.8mm is big, 56mm is med)
-wheel_diameter = 56
-wheel_circumference = 2*math.pi()*(wheel_diameter / 2)
+# wheel specifications (68.8mm is big, 56mm is medium)
+wheel_diameter = 68.8
+wheel_circumference = 2*3.1415926*(wheel_diameter / 2)
 rotations = distance_mm / wheel_circumference
-
+'''
 # move a set number of rotations
 def move(self, speed, rotations):
     self.on_for_rotations(speed,speed,rotations=rotations,brake=True)
-
+'''
 # run the specified number of laps while turning at each end
-for i in range(num_laps):
-    move(moveDA, speed_percent, rotations)
+for i in range(num_laps*2):
+    if(i % 2 == 0):
+        spkr.play_song((
+            ('D4', 'e'),
+            ('E4', 'e'),
+            ('F#4', 'e'),
+        ),tempo=240)
+        moveDA.on_for_rotations(speed_percent,speed_percent,rotations=rotations,brake=True)
+    else:
+        spkr.play_song((
+            ('D#4', 'e'),
+            ('E4', 'e'),
+            ('F#4', 'e'),
+        ),tempo=240)
+        moveDA.on_for_rotations(speed_percent,speed_percent,rotations=rotations*2,brake=True)
 
     # turn
-    moveDA.on_for_rotations(speed_percent,-speed_percent,rotations=1.5,brake=True)
+    spkr.play_song((
+        ('B4', 'e'),
+        ('B4', 'e'),
+    ),tempo=240)
+    moveDA.on_for_rotations(-speed_percent,speed_percent,rotations=1.35,brake=True)
